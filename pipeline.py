b80a3dde5c9528aa6468ee91e222a1aa92d31f2e
Jason Vriends <jason@thevriends.com>
2026-08-09 15:03:11 -0400

    Route rebuild preparation through durable manifests

--- pipeline.py
+++ pipeline.py
@@ -7,6 +7,12 @@ import sys
 
 
 def _main() -> int:
+    if len(sys.argv) >= 3 and sys.argv[1:3] == ["rebuild", "prepare"]:
+        from regdocs_atlas.rebuild_prepare_cli import main as rebuild_prepare_main
+        return rebuild_prepare_main(sys.argv[3:])
+    if len(sys.argv) >= 3 and sys.argv[1:3] == ["rebuild", "create"]:
+        from regdocs_atlas.rebuild_create_cli import main as rebuild_create_main
+        return rebuild_create_main(sys.argv[3:])
     if len(sys.argv) >= 4 and sys.argv[1:3] == ["recover", "scout"] and "--execute" in sys.argv[3:]:
         from regdocs_atlas.scout_recovery_cli import main as recovery_main
         return recovery_main(sys.argv[3:])
